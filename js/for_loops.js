dc6d9ca3baad8ccdf7a79598f64b0de22c79363e
francesperez <frances.perez141@gmail.com>
2022-08-18 10:27:44 -0500

    for loops completed assignmnet

---
+++ js/for_loops.js
@@ -0,0 +1,44 @@
+function showMultiplicationTable (x){
+    for(i = 1; i <= 10; i++) {
+        result = i * x;
+    console.log( `${x} * ${i} = ${result}`)
+}
+}
+
+let x
+for ( i = 0; i < 10; i++) {
+    x = Math.ceil(Math.random() * 181 + 20);
+    result = x % 2 === 0 ? x + " is even." : x + " is odd."; //called terniary
+    console.log(result)
+}
+
+// set up a for loop that iterates plus one
+// .length
+
+// for (let i = 1; i <= 9; i ++){
+//     let outputString = "";
+//     for (let count = 0; count < i; count ++){
+//         outputString = outputString + i;
+//     }
+//     console.log(outputString)
+// }
+
+// for (let i = 1; i <= 9; i++){
+//     console.log(i.toString().repeat(i));
+// }
+//
+//
+// for (let i = 100; i > 0; i -= 5){
+//     console.log(i);
+// }
+
+let string = " "
+for (let i = 0; i < 8; i ++){
+    let word = prompt("Let's make a sentence. Enter a word: ")
+    if (word === "curses"){
+        console.log("that word is not allowed.");
+        continue;
+    }
+    string += " " + word
+    console.log("Your sentence is:  " + string);
+}
