1b105e9bcc09749ffd39170e8a1539ec63ca9c3e
francesperez <frances.perez141@gmail.com>
2022-09-05 21:24:46 -0500

    Codewars 8kyu question: sumSquares

--- js/warmups.js
+++ js/warmups.js
@@ -137,3 +137,33 @@ function beeramid(bonus, price) {
 
 console.log(beeramid(1070,2));
 console.log(beeramid(9222,2));
+
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//
+//     For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+function squareSum(array) {
+    var sum = 0,
+        i = array.length;
+    while (i--)
+        sum += Math.pow(array[i], 2);
+    return sum;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
