be579f339b2fb5eb42700779b042a82a2b63ec02
francesperez <frances.perez141@gmail.com>
2022-09-14 13:51:26 -0500

    BOM/DOM Lecture notes pt.1

--- js/BOM-and-DOM-lecture.js
+++ js/BOM-and-DOM-lecture.js
@@ -1,11 +1,25 @@
-//----------------------------------------------BOM----------------------------------------------------------------
-//Did you know?...The browser is also an object that can be interacted with.
-//This is called the BOM: Browser Object Model
-//Most things that can be manipulated or done to the BOM is done using the window. object
-//the window. object can control the browser size, position, open up pop-up windows, display system dialogs, and more.
+// let sayHey = function() {
+//     let count = 0;
+//     function hey() {
+//         if (count > 10) {
+//             console.log('hey ' + count);
+//             count++
+//         } else {
+//             clearInterval();
+//         }
+//     }
+//     setInterval(hey, 5000);
+// }
 
+let count = 0;
+let thisTimeItWillStop;
+function hey(){
+    if (count < 11){
+        console.log("hey " + count);
+        count++
+    } else {
+        clearInterval(thisTimeItWillStop);
+    }
+}
+thisTimeItWillStop = setInterval(hey, 500);
 
-
-//----------------------------------------------DOM----------------------------------------------------------------
-// However, when it comes to Javascript, we mostly use DOM: Document Object Model, which is part of the BOM. It is
-// the ACTUAL website document. Most things within the DOM are done using the document. method
