9c046941f8ca3c1ee2e2e546953c854159fa3473
francesperez <frances.perez141@gmail.com>
2022-09-14 14:25:27 -0500

    Completed BOM exercise

--- js/BOM-and-DOM-lecture.js
+++ js/BOM-and-DOM-lecture.js
@@ -11,15 +11,35 @@
 //     setInterval(hey, 5000);
 // }
 
-let count = 0;
-let thisTimeItWillStop;
-function hey(){
-    if (count < 11){
-        console.log("hey " + count);
-        count++
-    } else {
-        clearInterval(thisTimeItWillStop);
-    }
+// let count = 0;
+// let thisTimeItWillStop;
+// function hey(){
+//     if (count < 11){
+//         console.log("hey " + count);
+//         count++
+//     } else {
+//         clearInterval(thisTimeItWillStop);
+//     }
+// }
+// thisTimeItWillStop = setInterval(hey, 500);
+
+// function tenHeys(){
+//     let count= 0;
+//     let thisWillStop = setInterval(function(){
+//         if (count < 10){
+//             console.log("hey " + count);
+//             count++;
+//         } else {
+//             clearInterval(thisWillStop);
+//         }
+//     }, 1000);
+// }
+
+setTimeout(wakeUpUser, 1000);
+
+function wakeUpUser(){
+    alert("WAKE UPPPP!!");
 }
-thisTimeItWillStop = setInterval(hey, 500);
+
+
 
