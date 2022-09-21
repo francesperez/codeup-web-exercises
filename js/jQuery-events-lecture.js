64f7654f19232f9b31a1870d212ad1d68b996c02
francesperez <frances.perez141@gmail.com>
2022-09-21 14:14:49 -0500

    Completed Mouse Events exercise from jQuery

---
+++ js/jQuery-events-lecture.js
@@ -0,0 +1,46 @@
+$(function(){
+      $("#clickMe").on('click', function (){
+         alert('yo!')
+      });
+
+      $('#create-an-element').on('click', function(){
+//.after is going to insert content after that element in the html
+         $(this).after("<button class ='will-this-work'>I'm after the last button</button>")
+      });
+
+//This won't work
+//The reason this won't work is because it is loading with the page. Once the page is done loading, it still hasn't existed yet until the button is clicked.
+      $(".will-this-work").on('click', function (){
+         alert(":( This won't work!")
+      });
+
+//This will work
+//It is called a delegated event
+      $("#dynamic-content").on('click', '.will-this-work', function (){
+         alert('ooooo yeeeeeeeeah');
+      });
+
+//We can add a class made in the css file to the document
+      $('.box').hover(function(){
+         $(this).addClass("pink");
+      },
+          function(){
+            $(this).removeClass('pink');
+      });
+
+//OLD CODE
+// $('.box').hover(function(){
+//         $(this).css('background-color', 'pink');
+//     },
+//     function(){
+//         $(this).css('background-color', 'unset')
+//     });
+
+    $(document).keydown(function(e){
+      alert('you just pressed a keydown!');
+        console.log(e.originalEvent.key);
+    });
+
+});
+
+
--- js/jQuery-exercises.js
+++ js/jQuery-exercises.js
@@ -6,7 +6,7 @@
 //ID Selectors
 
 //Use jQuery to select an element by the id. Alert the contents of the element.
-$(function () {
+ $(function () {
 //     let $h1alert = $('#h1').html();
 //     alert($h1alert);
 //
@@ -38,19 +38,46 @@ $(function () {
 //Remove your custom jQuery code from previous exercises.
 
 //Using jQuery, set the font-size of all li elements to 20px.
-$('li').css('font-size', '20px');
+// $('li').css('font-size', '20px');
 
 //MULTIPLE Selectors
 //Combine your selectors that highlight all the h1, p, and li elements.
 // Craft selectors that highlight all the h1, p, and li elements.
-$("h1, p, li").css('background-color', '#FFFF00');
+// $("h1, p, li").css('background-color', '#FFFF00');
 
 
 //Create a jQuery statement to alert the contents of your h1 element(s).
+//
+//     let $h1alert = $('#h1').html();
+//     alert($h1alert)
+//
 
-    let $h1alert = $('#h1').html();
-    alert($h1alert)
+
+
+//MOUSE EVENTS
+// Remove your custom jQuery code from previous exercises.
+
+//Add jQuery code that will change the background color of an h1 element when clicked.
+$('.pageTitle').click(function(){
+        $(this).css('background-color', 'pink');
+    })
+
+//Make all paragraphs have a font size of 18px when they are double clicked.
+     $('.codeup').dblclick(function(){
+         $(this).css('font-size', '18px');
+     })
+
+// Set all li text color to red when the mouse is hovering; reset to black when it is not.
+     $('.listdiv').hover(function(){
+        $(this).css('color', 'red');
+    },
+    function(){
+        $(this).css('color', 'unset')
+    });
+
+
+
+
+
 
 })
-
-
