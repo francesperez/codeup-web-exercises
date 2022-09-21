7f955a435e84627053c997729d16f47b9febc497
francesperez <frances.perez141@gmail.com>
2022-09-21 09:46:55 -0500

    jQuery exercise 1 completed: Alert pops up when page is done loading

--- js/jQuery-exercises.js
+++ js/jQuery-exercises.js
@@ -1,4 +1,3 @@
-$(function () {
-    //Here, we can use the # for the id selector; we can use .text to replace the text
-    alert($("Page loaded"))
-})
+$( document ).ready(function() {
+    alert( "ready!" );
+});
