f394485b53106456eb6f7b06c31e76ebdaf3d034
francesperez <frances.perez141@gmail.com>
2022-09-21 10:05:53 -0500

    Part 1: ID Selectors completed from jQuery selectors exercise

--- js/jQuery-exercises.js
+++ js/jQuery-exercises.js
@@ -1,3 +1,30 @@
-$( document ).ready(function() {
-    alert( "ready!" );
-});
+// $( document ).ready(function() {
+//     alert( "ready!" );
+// });
+
+
+//ID Selectors
+
+//Use jQuery to select an element by the id. Alert the contents of the element.
+// $(function () {
+//     let $h1alert = $('#h1').html();
+//     alert($h1alert);
+// })
+
+//Update the jQuery code to select and alert a different id.
+// $(function () {
+//     let $ipsum = $('#lorem').html();
+//     alert($ipsum);
+// })
+
+//Use the same id on 2 elements. How does this change the jQuery selection?
+//On top of getting a red line, I got an undefined alert, then the page alerted the first function.
+
+//Remove the duplicate id. Each id should be unique on that page.
+
+
+//CLASS Selectors
+//Remove your custom jQuery code from previous exercises.
+//Update your code so that at least 3 different elements have the same class named codeup.
+
+$(".codeup").css('border', '1px solid red');
