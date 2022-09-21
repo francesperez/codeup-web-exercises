1498b9d169c41e3e35933abd2c9f95d69452a9ad
francesperez <frances.perez141@gmail.com>
2022-09-21 10:16:33 -0500

    Part 3: Element Selectors completed from jQuery selectors exercise

--- js/jQuery-exercises.js
+++ js/jQuery-exercises.js
@@ -28,8 +28,25 @@
 //Update your code so that at least 3 different elements have the same class named codeup.
 
 //Using jQuery, create a border around all elements with the class codeup that is 1 pixel wide and red.
-$(".codeup").css('border', '1px solid red');
+//$(".codeup").css('border', '1px solid red');
 
 //Remove the class from one of the elements. Refresh and test that the border has been removed.
 //Give another element an id of codeup. Does this element get a border now?
     //Yes
+
+//ELEMENT Selectors
+//Remove your custom jQuery code from previous exercises.
+
+//Using jQuery, set the font-size of all li elements to 20px.
+$('li').css('font-size', '20px');
+
+//Craft selectors that highlight all the h1, p, and li elements.
+$("h1, p, li").css('background-color', '#FFFF00');
+
+
+//Create a jQuery statement to alert the contents of your h1 element(s).
+$(function () {
+    let $h1alert = $('#h1').html();
+    alert($h1alert);
+})
+
