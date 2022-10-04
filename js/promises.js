35fc5e6aeb68be451c88558739d40e331a3f07a8
francesperez <frances.perez141@gmail.com>
2022-10-04 11:39:08 -0500

    Promises lecture without bonus

--- js/promises.js
+++ js/promises.js
@@ -6,3 +6,12 @@ fetch('https://api.github.com/users/francesperez/events', {
 }).then( response => response.json())
     .then(data => console.log(data[0].created_at))
     .catch( error => console.error(error));
+
+//Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+wait(1000).then(() => console.log("You'll see this after 1 second"));
+wait(3000).then(() => console.log("You'll see this after 3 seconds"));
